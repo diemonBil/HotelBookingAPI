d387a2d69d90c2c5f22800241aa6ed669840eaad
diemonBil <crazyevildocs@gmail.com>
2025-05-04 12:25:33 +0300

    connect to cloud PostgreSQL (AWS RDS) and migrate data from local SQLite

--- user/urls.py
+++ user/urls.py
@@ -11,7 +11,7 @@ from rest_framework_simplejwt.views import (
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
-    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('me/', ManageUserView.as_view(), name='me'),
 ]
